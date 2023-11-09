52b8444f0d1ccf458275d45092754359933845ab
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-11-09 08:21:49 -0500

    sw

--- TextEdit.py
+++ TextEdit.py
@@ -3,49 +3,50 @@ from tkinter import *
 from tkinter import font
 import pyperclip
 
+
 root = Tk()
 root.geometry("300x300")  # Set window size
 root.title("Text Editor 📃")  # Set window title
 root.resizable(False, False)  # Disable resizing
 
 def saved():
-    pyperclip.copy(text.get("1.0", END))
-    
-    if color_var.get() == "Black":
-        print("Test Success")
-        text.config(foreground="Black")
-    elif color_var.get() == "Red":
-        print("Test Success")
-        text.config(foreground="Red")
-    elif color_var.get() == "Blue":
-        print("Test Success")
-        text.config(foreground="Blue")
+    text_content = text.get("1.0", END)
+    pyperclip.copy(text_content)  # Copy text to clipboard
 
-    if font_var.get() == "Comic Sans MS":
-        text.config(font=("Comic Sans MS", 12)) # replace "Comic Sans MS
-    elif font_var.get() == "Verdana":
+    selected_font = font_var.get()
+    if selected_font == "Comic Sans MS":
+        text.config(font=("Comic Sans MS", 12))
+    elif selected_font == "Verdana":
         text.config(font=("Verdana", 12))
-    elif font_var.get() == "Arial":
+    elif selected_font == "Arial":
         text.config(font=("Arial", 12))
 
-    print("Exit Code 0")
+    selected_color = color_var.get()
+    if selected_color == "Black":
+        text.config(foreground="Black")
+    elif selected_color == "Red":
+        text.config(foreground="Red")
+    elif selected_color == "Blue":
+        text.config(foreground="Blue")
+
+    state = Label(root, text="Copied to Clipboard 📋")
+    state.place(x=90, y=140)
+    state.after(3000, state.destroy)
+    root.update()
 
 
-    
-    
 text = Text(root)
 text.place(width=300, height=250, y=30)
 
 font_var = StringVar(root)
-font_var.set("Font 🔤")
+font_var.set("Arial")
 font = OptionMenu(root, font_var, "Arial", "Comic Sans MS", "Verdana")
 font.place(x=0, width=150)
 
 color_var = StringVar(root)
-color_var.set("Text Color 🌈")
+color_var.set("Black")
 color = OptionMenu(root, color_var, "Black", "Red", "Blue")
 color.place(x=150, width=150)
-
 
 changes = Button(root, text="Save Changes ✔", command=saved)
 changes.place(y=275, width=300)
